8b137949c97aefd1b0aefde97377e44a48270151
AntonMorozov1986 <75334152+AntonMorozov1986@users.noreply.github.com>
2021-07-15 00:18:32 +0300

    Первоначальная настройка
    1) Установлены необходимые пакеты.
    2) Настроен webpack
    3) Собрано простейшее приложение для проверки

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>{{ greetings }}</div>
+    <button @click="greetings = 'Hello Smith'">change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data () {
+    return {
+      greetings: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  context: path.join(__dirname, 'src'),
+  entry: './main.js',
+  devtool: 'inline-source-map',
+
+  resolve: {
+    extensions: ['.vue', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ]
+};
